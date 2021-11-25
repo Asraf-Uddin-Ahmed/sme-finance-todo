d4c9523e284169e8a55f9ef65c16dde26b1e3c1d
Asraf Uddin Ahmed <aahmed@ejogajogbd.com>
2021-11-25 22:33:32 +0600

    Initialize Spring boot project

---
+++ src/main/java/com/smefinance/todo/SmeFinanceTodoApplication.java
@@ -0,0 +1,13 @@
+package com.smefinance.todo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmeFinanceTodoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SmeFinanceTodoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/smefinance/todo/SmeFinanceTodoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.smefinance.todo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmeFinanceTodoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
