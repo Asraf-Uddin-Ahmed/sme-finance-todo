46003eed87f06b9d757754dbed8865d460bf52c0
Asraf Uddin Ahmed <aahmed@ejogajogbd.com>
2021-11-26 09:54:26 +0600

    Add todo task controller with save action method

---
+++ src/main/java/com/smefinance/todo/controllers/TodoTaskController.java
@@ -0,0 +1,41 @@
+package com.smefinance.todo.controllers;
+
+import javax.validation.Valid;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.smefinance.todo.dtos.mapper.TodoTaskMapper;
+import com.smefinance.todo.dtos.request.TodoTaskRequest;
+import com.smefinance.todo.entities.TodoTask;
+import com.smefinance.todo.services.TodoTaskService;
+
+import lombok.extern.log4j.*;
+
+@RestController
+@RequestMapping("/todo-tasks")
+public class TodoTaskController {
+	
+	private TodoTaskService todoTaskService;
+	private TodoTaskMapper todoTaskMapper;
+	
+	@Autowired
+	public TodoTaskController(TodoTaskService todoTaskService, TodoTaskMapper todoTaskMapper) {
+		this.todoTaskService = todoTaskService;
+		this.todoTaskMapper = todoTaskMapper;
+	}
+	
+	@PostMapping("")
+	@ResponseStatus(HttpStatus.CREATED)
+	public TodoTask saveTodoTask(@RequestBody @Valid TodoTaskRequest request) {
+		TodoTask todoTask = this.todoTaskMapper.getEntity(request);
+		return this.todoTaskService.save(todoTask);
+	}
+
+}
