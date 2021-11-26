fc97d9ca373be4d2642c6b5d985b20fac11b3d09
Asraf Uddin Ahmed <aahmed@ejogajogbd.com>
2021-11-26 08:40:07 +0600

    Add request DTO with validation for todo task

---
+++ src/main/java/com/smefinance/todo/dtos/request/TodoTaskRequest.java
@@ -0,0 +1,25 @@
+package com.smefinance.todo.dtos.request;
+
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+
+import com.smefinance.todo.contants.PriorityType;
+import com.smefinance.todo.contants.TaskStatus;
+
+import lombok.Data;
+
+@Data
+public class TodoTaskRequest {
+
+	@NotBlank
+	private String title;
+	
+	private String description;
+	
+	@NotNull
+	private PriorityType priority;
+	
+	@NotNull
+	private TaskStatus status;
+	
+}
