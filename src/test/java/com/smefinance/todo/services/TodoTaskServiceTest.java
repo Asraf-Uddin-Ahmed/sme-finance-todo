7903879ecf4c8e7734f003b6c44dcba811e730d5
Asraf Uddin Ahmed <aahmed@ejogajogbd.com>
2021-12-03 16:32:06 +0600

    Update assertion method for handling exception and remove code smell from the TodoTaskServiceTest

--- src/test/java/com/smefinance/todo/services/TodoTaskServiceTest.java
+++ src/test/java/com/smefinance/todo/services/TodoTaskServiceTest.java
@@ -1,92 +1,90 @@
 package com.smefinance.todo.services;
 
-import static org.assertj.core.api.Assertions.assertThat;
-import static org.assertj.core.api.Assertions.assertThatExceptionOfType;
-import static org.mockito.Mockito.when;
-
-import java.util.Collections;
-import java.util.List;
-
-import static org.mockito.ArgumentMatchers.any;
-
+import com.smefinance.todo.entities.TodoTask;
+import com.smefinance.todo.exception.ResourceNotFoundException;
+import com.smefinance.todo.repositories.TodoTaskRepository;
+import com.smefinance.todo.services.imp.TodoTaskServiceImpl;
 import org.junit.jupiter.api.Test;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.springframework.boot.test.context.SpringBootTest;
 
-import com.smefinance.todo.entities.TodoTask;
-import com.smefinance.todo.exception.ResourceNotFoundException;
-import com.smefinance.todo.repositories.TodoTaskRepository;
-import com.smefinance.todo.services.imp.TodoTaskServiceImpl;
+import java.util.Collections;
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.AssertionsForClassTypes.assertThatThrownBy;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.when;
 
 @SpringBootTest
 class TodoTaskServiceTest {
 
-	@InjectMocks
+    @InjectMocks
     private TodoTaskServiceImpl todoTaskService;
 
-	@Mock
-	private TodoTaskRepository repository;
-	
-	@Test
-	public void whenSaveTodoTask_thenTaskShouldBeFound() {
-		TodoTask givenTodoTask = new TodoTask();
-		when(repository.save(any())).thenReturn(givenTodoTask);
-        
-		TodoTask foundTodoTask = todoTaskService.save(new TodoTask());
-		
-		assertThat(foundTodoTask).isEqualTo(givenTodoTask);
-	}
+    @Mock
+    private TodoTaskRepository repository;
 
-	@Test
-	public void whenSaveWithTaskStatus_thenTaskShouldBeFound() {
-		TodoTask givenTodoTask = new TodoTask();
-		when(repository.save(any())).thenReturn(givenTodoTask);
-        
-		TodoTask foundTodoTask = todoTaskService.saveWithTaskStatus(new TodoTask(), null);
-		
-		assertThat(foundTodoTask).isEqualTo(givenTodoTask);
-	}
-	
-	@Test
-	public void whenDeleteTheTask_thenTaskShouldBeMarkAsDeleted() {
-		TodoTask givenTodoTask = new TodoTask();
-		when(repository.save(any())).thenReturn(givenTodoTask);
-        
-		todoTaskService.delete(givenTodoTask);
-        
-		assertThat(givenTodoTask.isDeleted()).isTrue();
-	}
-	
-	@Test
-	public void whenGetAllByTaskStatus_thenTaskShouldBeFound() {
-		TodoTask givenTodoTask = new TodoTask();
-		when(repository.findAllBy(any())).thenReturn(Collections.singletonList(givenTodoTask));
-        
-		List<TodoTask> foundTodoTasks = todoTaskService.getAllBy(null);
-		
-		assertThat(foundTodoTasks.get(0)).isEqualTo(givenTodoTask);
-	}
-	
-	@Test
-	public void whenGetByIdTaskStatus_thenTaskShouldBeFound() {
-		TodoTask givenTodoTask = new TodoTask();
-		givenTodoTask.setId(0);
-		when(repository.findByIdAndIsDeletedFalse(givenTodoTask.getId())).thenReturn(givenTodoTask);
-        
-		TodoTask foundTodoTask = todoTaskService.getById(givenTodoTask.getId());
-		
-		assertThat(foundTodoTask).isEqualTo(givenTodoTask);
-	}
-	
-	@Test
-	public void whenGetByIdNotFound_thenThrowException() {
-		TodoTask givenTodoTask = new TodoTask();
-		givenTodoTask.setId(0);
-		when(repository.findByIdAndIsDeletedFalse(givenTodoTask.getId())).thenReturn(null);
-        
-		assertThatExceptionOfType(ResourceNotFoundException.class)
-			.isThrownBy(() -> todoTaskService.getById(givenTodoTask.getId()));
-	}
-	
+    @Test
+    void whenSaveTodoTask_thenTaskShouldBeFound() {
+        TodoTask givenTodoTask = new TodoTask();
+        when(repository.save(any())).thenReturn(givenTodoTask);
+
+        TodoTask foundTodoTask = todoTaskService.save(new TodoTask());
+
+        assertThat(foundTodoTask).isEqualTo(givenTodoTask);
+    }
+
+    @Test
+    void whenSaveWithTaskStatus_thenTaskShouldBeFound() {
+        TodoTask givenTodoTask = new TodoTask();
+        when(repository.save(any())).thenReturn(givenTodoTask);
+
+        TodoTask foundTodoTask = todoTaskService.saveWithTaskStatus(new TodoTask(), null);
+
+        assertThat(foundTodoTask).isEqualTo(givenTodoTask);
+    }
+
+    @Test
+    void whenDeleteTheTask_thenTaskShouldBeMarkAsDeleted() {
+        TodoTask givenTodoTask = new TodoTask();
+        when(repository.save(any())).thenReturn(givenTodoTask);
+
+        todoTaskService.delete(givenTodoTask);
+
+        assertThat(givenTodoTask.isDeleted()).isTrue();
+    }
+
+    @Test
+    void whenGetAllByTaskStatus_thenTaskShouldBeFound() {
+        TodoTask givenTodoTask = new TodoTask();
+        when(repository.findAllBy(any())).thenReturn(Collections.singletonList(givenTodoTask));
+
+        List<TodoTask> foundTodoTasks = todoTaskService.getAllBy(null);
+
+        assertThat(foundTodoTasks.get(0)).isEqualTo(givenTodoTask);
+    }
+
+    @Test
+    void whenGetByIdTaskStatus_thenTaskShouldBeFound() {
+        TodoTask givenTodoTask = new TodoTask();
+        givenTodoTask.setId(0);
+        when(repository.findByIdAndIsDeletedFalse(givenTodoTask.getId())).thenReturn(givenTodoTask);
+
+        TodoTask foundTodoTask = todoTaskService.getById(givenTodoTask.getId());
+
+        assertThat(foundTodoTask).isEqualTo(givenTodoTask);
+    }
+
+    @Test
+    void whenGetByIdNotFound_thenThrowException() {
+        TodoTask givenTodoTask = new TodoTask();
+        givenTodoTask.setId(0);
+        when(repository.findByIdAndIsDeletedFalse(givenTodoTask.getId())).thenReturn(null);
+
+        assertThatThrownBy(() -> todoTaskService.getById(givenTodoTask.getId()))
+                .isInstanceOf(ResourceNotFoundException.class);
+    }
+
 }
