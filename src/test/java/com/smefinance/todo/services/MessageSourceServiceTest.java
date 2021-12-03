f846aa238186607715d77667107744c1c52821aa
Asraf Uddin Ahmed <aahmed@ejogajogbd.com>
2021-12-03 16:30:47 +0600

    Remove code smell from the MessageSourceServiceTest

--- src/test/java/com/smefinance/todo/services/MessageSourceServiceTest.java
+++ src/test/java/com/smefinance/todo/services/MessageSourceServiceTest.java
@@ -1,10 +1,7 @@
 package com.smefinance.todo.services;
 
-import static org.assertj.core.api.Assertions.assertThat;
-import static org.mockito.Mockito.when;
-
-import static org.mockito.ArgumentMatchers.any;
-
+import com.smefinance.todo.config.CustomLocaleResolver;
+import com.smefinance.todo.services.imp.MessageSourceServiceImpl;
 import org.junit.jupiter.api.Test;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
@@ -12,40 +9,41 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.context.MessageSource;
 import org.springframework.context.i18n.LocaleContextHolder;
 
-import com.smefinance.todo.config.CustomLocaleResolver;
-import com.smefinance.todo.services.imp.MessageSourceServiceImpl;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.when;
 
 @SpringBootTest
 class MessageSourceServiceTest {
 
-	@InjectMocks
+    @InjectMocks
     private MessageSourceServiceImpl messageSourceService;
 
-	@Mock
-	private MessageSource messageSource;
-	@Mock
-	private CustomLocaleResolver customLocaleResolver;
-	
-	@Test
-	public void whenPropertyProvided_thenGetReleventMessage() {
-		String givenProperty = "prop.key";
-		String givenMessage = "foo bar";
-		when(messageSource.getMessage(givenProperty, null, LocaleContextHolder.getLocale())).thenReturn(givenMessage);
-        
-		String foundMessage = messageSourceService.getMessage(givenProperty);
-		
-		assertThat(foundMessage).isEqualTo(givenMessage);
-	}
+    @Mock
+    private MessageSource messageSource;
+    @Mock
+    private CustomLocaleResolver customLocaleResolver;
 
-	@Test
-	public void whenPropertyAndHttpServletRequestProvided_thenGetReleventMessage() {
-		String givenProperty = "prop.key";
-		String givenMessage = "foo bar";
-		when(messageSource.getMessage(givenProperty, null, customLocaleResolver.resolveLocale(any()))).thenReturn(givenMessage);
-        
-		String foundMessage = messageSourceService.getMessage(givenProperty, null);
-		
-		assertThat(foundMessage).isEqualTo(givenMessage);
-	}
+    @Test
+    void whenPropertyProvided_thenGetRelevantMessage() {
+        String givenProperty = "prop.key";
+        String givenMessage = "foo bar";
+        when(messageSource.getMessage(givenProperty, null, LocaleContextHolder.getLocale())).thenReturn(givenMessage);
+
+        String foundMessage = messageSourceService.getMessage(givenProperty);
+
+        assertThat(foundMessage).isEqualTo(givenMessage);
+    }
+
+    @Test
+    void whenPropertyAndHttpServletRequestProvided_thenGetRelevantMessage() {
+        String givenProperty = "prop.key";
+        String givenMessage = "foo bar";
+        when(messageSource.getMessage(givenProperty, null, customLocaleResolver.resolveLocale(any()))).thenReturn(givenMessage);
+
+        String foundMessage = messageSourceService.getMessage(givenProperty, null);
+
+        assertThat(foundMessage).isEqualTo(givenMessage);
+    }
 
 }
