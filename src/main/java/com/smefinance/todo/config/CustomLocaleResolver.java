7cc7edf3c1b4f543af8f019444a396e2844a4969
Asraf Uddin Ahmed <aahmed@ejogajogbd.com>
2021-12-03 15:54:42 +0600

    Remove code smell from CustomLocaleResolver

--- src/main/java/com/smefinance/todo/config/CustomLocaleResolver.java
+++ src/main/java/com/smefinance/todo/config/CustomLocaleResolver.java
@@ -1,35 +1,34 @@
 package com.smefinance.todo.config;
 
-import java.util.Arrays;
-import java.util.List;
-import java.util.Locale;
-
-import javax.servlet.http.HttpServletRequest;
-
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.support.ResourceBundleMessageSource;
 import org.springframework.web.servlet.i18n.AcceptHeaderLocaleResolver;
 
+import javax.servlet.http.HttpServletRequest;
+import java.util.Collections;
+import java.util.List;
+import java.util.Locale;
+
 @Configuration
 public class CustomLocaleResolver extends AcceptHeaderLocaleResolver {
 
-	List<Locale> LOCALES = Arrays.asList(new Locale("en"));
+    private final List<Locale> locales = Collections.singletonList(new Locale("en"));
 
-	@Override
-	public Locale resolveLocale(HttpServletRequest request) {
-		String headerLang = request.getHeader("Accept-Language");
-		return headerLang == null || headerLang.isEmpty() ? Locale.getDefault()
-				: Locale.lookup(Locale.LanguageRange.parse(headerLang), LOCALES);
-	}
+    @Override
+    public Locale resolveLocale(HttpServletRequest request) {
+        String headerLang = request.getHeader("Accept-Language");
+        return headerLang == null || headerLang.isEmpty() ? Locale.getDefault()
+                : Locale.lookup(Locale.LanguageRange.parse(headerLang), locales);
+    }
 
-	@Bean
-	public ResourceBundleMessageSource messageSource() {
-		ResourceBundleMessageSource rs = new ResourceBundleMessageSource();
-		rs.setBasename("messages");
-		rs.setDefaultEncoding("UTF-8");
-		rs.setUseCodeAsDefaultMessage(true);
-		return rs;
-	}
-	
+    @Bean
+    public ResourceBundleMessageSource messageSource() {
+        ResourceBundleMessageSource rs = new ResourceBundleMessageSource();
+        rs.setBasename("messages");
+        rs.setDefaultEncoding("UTF-8");
+        rs.setUseCodeAsDefaultMessage(true);
+        return rs;
+    }
+
 }
