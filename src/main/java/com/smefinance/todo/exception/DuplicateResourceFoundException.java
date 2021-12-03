5c7b0a6b8d6206484f7cc1f123913b6b0263c195
Asraf Uddin Ahmed <aahmed@ejogajogbd.com>
2021-12-03 17:37:41 +0600

    Remove unnecessary casting and parameter with code smells

--- src/main/java/com/smefinance/todo/exception/DuplicateResourceFoundException.java
+++ src/main/java/com/smefinance/todo/exception/DuplicateResourceFoundException.java
@@ -1,35 +1,31 @@
 package com.smefinance.todo.exception;
 
+import com.smefinance.todo.contants.ErrorCode;
+import org.apache.commons.lang3.StringUtils;
+
 import java.util.HashMap;
 import java.util.Map;
 import java.util.stream.IntStream;
 
-import org.apache.commons.lang3.StringUtils;
-
-import com.smefinance.todo.contants.ErrorCode;
-
 
 public class DuplicateResourceFoundException extends RuntimeException {
 
-	/**
-	 * 
-	 */
-	private static final long serialVersionUID = 1L;
+    private static final long serialVersionUID = 1L;
 
-	public DuplicateResourceFoundException(Class<?> clazz, String... searchParamsMap) {
-		super(DuplicateResourceFoundException.generateMessage(clazz.getSimpleName(),
-				toMap(String.class, String.class, searchParamsMap)));
-	}
+    public DuplicateResourceFoundException(Class<?> clazz, String... searchParamsMap) {
+        super(DuplicateResourceFoundException.generateMessage(clazz.getSimpleName(),
+                toMap(searchParamsMap)));
+    }
 
-	private static String generateMessage(String entity, Map<String, String> searchParams) {
-		return "Duplicate " + StringUtils.capitalize(entity) + " found for parameters " + searchParams;
-	}
+    private static String generateMessage(String entity, Map<String, String> searchParams) {
+        return "Duplicate " + StringUtils.capitalize(entity) + " found for parameters " + searchParams;
+    }
 
-	private static <K, V> Map<K, V> toMap(Class<K> keyType, Class<V> valueType, String... entries) {
-		if (entries.length % 2 == 1)
-			throw new IllegalArgumentException(ErrorCode.Exception.Resource.DuplicateFound.Invalid.ENTRIES);
-		return IntStream.range(0, entries.length / 2).map(i -> i * 2).collect(HashMap::new,
-				(m, i) -> m.put(keyType.cast(entries[i]), valueType.cast(entries[i + 1])), Map::putAll);
-	}
+    private static Map<String, String> toMap(String... entries) {
+        if (entries.length % 2 == 1)
+            throw new IllegalArgumentException(ErrorCode.Exception.Resource.DuplicateFound.Invalid.ENTRIES);
+        return IntStream.range(0, entries.length / 2).map(i -> i * 2).collect(HashMap::new,
+                (m, i) -> m.put(entries[i], entries[i + 1]), Map::putAll);
+    }
 
 }
