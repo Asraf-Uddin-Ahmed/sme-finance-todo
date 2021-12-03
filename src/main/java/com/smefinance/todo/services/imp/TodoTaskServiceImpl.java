e845dee506b8c2b82e8838156dde9cbfc3b1e0a0
Asraf Uddin Ahmed <aahmed@ejogajogbd.com>
2021-12-03 15:42:24 +0600

    Place the Transactional annotation in method level from class level

--- src/main/java/com/smefinance/todo/services/imp/TodoTaskServiceImpl.java
+++ src/main/java/com/smefinance/todo/services/imp/TodoTaskServiceImpl.java
@@ -1,62 +1,63 @@
 package com.smefinance.todo.services.imp;
 
-import java.util.List;
-import java.util.NoSuchElementException;
-
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Service;
-import org.springframework.transaction.annotation.Transactional;
-
 import com.smefinance.todo.contants.TaskStatus;
 import com.smefinance.todo.entities.TodoTask;
 import com.smefinance.todo.repositories.TodoTaskRepository;
 import com.smefinance.todo.services.TodoTaskService;
 import com.smefinance.todo.utils.ExceptionPreconditions;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+import java.util.NoSuchElementException;
 
 @Service
-@Transactional
 public class TodoTaskServiceImpl implements TodoTaskService {
 
-	private TodoTaskRepository todoTaskRepository;
+    private TodoTaskRepository todoTaskRepository;
 
-	@Autowired
-	public TodoTaskServiceImpl(TodoTaskRepository todoTaskRepository) {
-		this.todoTaskRepository = todoTaskRepository;
-	}
+    @Autowired
+    public TodoTaskServiceImpl(TodoTaskRepository todoTaskRepository) {
+        this.todoTaskRepository = todoTaskRepository;
+    }
 
-	@Override
-	public TodoTask saveWithTaskStatus(TodoTask todoTask, TaskStatus taskStatus) {
-		todoTask.setStatus(taskStatus);
-		return this.todoTaskRepository.save(todoTask);
-	}
+    @Override
+    @Transactional
+    public TodoTask saveWithTaskStatus(TodoTask todoTask, TaskStatus taskStatus) {
+        todoTask.setStatus(taskStatus);
+        return this.todoTaskRepository.save(todoTask);
+    }
 
-	@Override
-	public TodoTask save(TodoTask todoTask) {
-		return this.todoTaskRepository.save(todoTask);
-	}
+    @Override
+    @Transactional
+    public TodoTask save(TodoTask todoTask) {
+        return this.todoTaskRepository.save(todoTask);
+    }
 
-	@Override
-	public TodoTask getById(Integer id) {
-		try {
-			TodoTask todoTask = todoTaskRepository.findByIdAndIsDeletedFalse(id);
-			if(todoTask == null) {
-				return ExceptionPreconditions.entityNotFound(TodoTask.class, "id", id.toString());	
-			}
-			return todoTask;
-		} catch (NoSuchElementException nseex) {
-			return ExceptionPreconditions.entityNotFound(TodoTask.class, "id", id.toString());
-		}
-	}
-	
-	@Override
-	public void delete(TodoTask todoTask) {
-		todoTask.setDeleted(true);
-		this.todoTaskRepository.save(todoTask);
-	}
-	
-	@Override
-	public List<TodoTask> getAllBy(TaskStatus status) {
-		return this.todoTaskRepository.findAllBy(status);
-	}
-	
+    @Override
+    public TodoTask getById(Integer id) {
+        try {
+            TodoTask todoTask = todoTaskRepository.findByIdAndIsDeletedFalse(id);
+            if (todoTask == null) {
+                return ExceptionPreconditions.entityNotFound(TodoTask.class, "id", id.toString());
+            }
+            return todoTask;
+        } catch (NoSuchElementException nseex) {
+            return ExceptionPreconditions.entityNotFound(TodoTask.class, "id", id.toString());
+        }
+    }
+
+    @Override
+    @Transactional
+    public void delete(TodoTask todoTask) {
+        todoTask.setDeleted(true);
+        this.todoTaskRepository.save(todoTask);
+    }
+
+    @Override
+    public List<TodoTask> getAllBy(TaskStatus status) {
+        return this.todoTaskRepository.findAllBy(status);
+    }
+
 }
