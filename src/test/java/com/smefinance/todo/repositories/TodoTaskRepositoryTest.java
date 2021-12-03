43f672f093f2c73868e36618cb1b3a4574139369
Asraf Uddin Ahmed <aahmed@ejogajogbd.com>
2021-12-03 16:14:35 +0600

    Update an unit test method to test with optional field

--- src/test/java/com/smefinance/todo/repositories/TodoTaskRepositoryTest.java
+++ src/test/java/com/smefinance/todo/repositories/TodoTaskRepositoryTest.java
@@ -1,68 +1,68 @@
 package com.smefinance.todo.repositories;
 
-import static org.assertj.core.api.Assertions.assertThat;
-
-import java.util.List;
-
+import com.smefinance.todo.contants.PriorityType;
+import com.smefinance.todo.contants.TaskStatus;
+import com.smefinance.todo.entities.TodoTask;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
 
-import com.smefinance.todo.contants.PriorityType;
-import com.smefinance.todo.contants.TaskStatus;
-import com.smefinance.todo.entities.TodoTask;
+import java.util.List;
+import java.util.Optional;
+
+import static org.assertj.core.api.Assertions.assertThat;
 
 @DataJpaTest
 class TodoTaskRepositoryTest {
 
-	@Autowired
+    @Autowired
     private TestEntityManager entityManager;
 
     @Autowired
     private TodoTaskRepository repository;
 
     private TodoTask getTodoTask(TaskStatus taskStatus) {
-    	TodoTask todoTask = new TodoTask();
-    	todoTask.setDescription("test description");
-    	todoTask.setPriority(PriorityType.MEDIUM);
-    	todoTask.setStatus(taskStatus);
-    	todoTask.setTitle("test title");
-    	return todoTask;
+        TodoTask todoTask = new TodoTask();
+        todoTask.setDescription("test description");
+        todoTask.setPriority(PriorityType.MEDIUM);
+        todoTask.setStatus(taskStatus);
+        todoTask.setTitle("test title");
+        return todoTask;
     }
-    
+
     private TodoTask getTodoTask() {
-    	return this.getTodoTask(TaskStatus.DOING);
+        return this.getTodoTask(TaskStatus.TODO);
     }
-    
-	@Test
-	void test_save() {
-		TodoTask todoTask = this.getTodoTask();
-		TodoTask savedData = entityManager.persist(todoTask);
-		
-		TodoTask foundData = repository.findById(savedData.getId()).get();
-		
-		assertThat(foundData).isEqualTo(savedData);
-	}
 
-	@Test
-	void test_findByIdAndIsDeletedFalse() {
-		TodoTask todoTask = this.getTodoTask();
-		TodoTask savedData = entityManager.persist(todoTask);
-		
-		TodoTask foundData = repository.findByIdAndIsDeletedFalse(savedData.getId());
-		
-		assertThat(foundData).isEqualTo(savedData);
-	}
+    @Test
+    void test_save() {
+        TodoTask todoTask = this.getTodoTask();
+        TodoTask savedData = entityManager.persist(todoTask);
 
-	@Test
-	void test_findAllBy() {
-		TodoTask todoTask = this.getTodoTask(TaskStatus.DOING);
-		TodoTask savedData = entityManager.persist(todoTask);
-		
-		List<TodoTask> foundDataList = repository.findAllBy(TaskStatus.DOING);
-		
-		assertThat(foundDataList.get(0)).isEqualTo(savedData);
-	}
-	
+        Optional<TodoTask> foundDataOptional = repository.findById(savedData.getId());
+
+        assertThat(foundDataOptional).isPresent().get().isEqualTo(savedData);
+    }
+
+    @Test
+    void test_findByIdAndIsDeletedFalse() {
+        TodoTask todoTask = this.getTodoTask();
+        TodoTask savedData = entityManager.persist(todoTask);
+
+        TodoTask foundData = repository.findByIdAndIsDeletedFalse(savedData.getId());
+
+        assertThat(foundData).isEqualTo(savedData);
+    }
+
+    @Test
+    void test_findAllBy() {
+        TodoTask todoTask = this.getTodoTask(TaskStatus.DOING);
+        TodoTask savedData = entityManager.persist(todoTask);
+
+        List<TodoTask> foundDataList = repository.findAllBy(TaskStatus.DOING);
+
+        assertThat(foundDataList.get(0)).isEqualTo(savedData);
+    }
+
 }
