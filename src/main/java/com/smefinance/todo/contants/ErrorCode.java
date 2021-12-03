92e1398fd678728495dbf13508f87c1d7ef8fdaf
Asraf Uddin Ahmed <aahmed@ejogajogbd.com>
2021-12-03 20:45:12 +0600

    Update all keys in messages.properties with values of ErrorCode

--- src/main/java/com/smefinance/todo/contants/ErrorCode.java
+++ src/main/java/com/smefinance/todo/contants/ErrorCode.java
@@ -3,17 +3,17 @@ package com.smefinance.todo.contants;
 
 public class ErrorCode {
 
-    public static final String HTTP_ERROR_MISSING_SERVLET_REQUEST_PARAMETER = "httperror.missingservletrequestparameter";
-    public static final String HTTP_ERROR_MESSAGE_NOT_READABLE = "httperror.message.notreadable";
-    public static final String HTTP_ERROR_MESSAGE_NOT_WRITABLE = "httperror.message.notwritable";
+    public static final String HTTP_ERROR_MISSING_SERVLET_REQUEST_PARAMETER = "http-error.missing-servlet-request-parameter";
+    public static final String HTTP_ERROR_MESSAGE_NOT_READABLE = "http-error.message.not-readable";
+    public static final String HTTP_ERROR_MESSAGE_NOT_WRITABLE = "http-error.message.not-writable";
     public static final String EXCEPTION = "exception";
-    public static final String EXCEPTION_RESOURCE_DUPLICATE_FOUND = "exception.resource.duplicatefound";
-    public static final String EXCEPTION_RESOURCE_DUPLICATE_FOUND_INVALID_ENTRIES = "exception.resource.duplicatefound.invalid.entries";
-    public static final String EXCEPTION_RESOURCE_NOT_FOUND = "exception.resource.notfound";
-    public static final String EXCEPTION_DATA_INTEGRITY_VIOLATION = "exception.dataintegrityviolation";
-    public static final String EXCEPTION_DATA_INTEGRITY_VIOLATION_CONSTRAINT_VIOLATION = "exception.dataintegrityviolation.constraintviolation";
-    public static final String EXCEPTION_METHOD_ARGUMENT_TYPE_MISMATCH = "exception.methodargumenttypemismatch";
-    public static final String EXCEPTION_ILLEGAL_ARGUMENT = "exception.illegalargument";
+    public static final String EXCEPTION_RESOURCE_DUPLICATE_FOUND = "exception.resource.duplicate-found";
+    public static final String EXCEPTION_RESOURCE_DUPLICATE_FOUND_INVALID_ENTRIES = "exception.resource.duplicate-found.invalid.entries";
+    public static final String EXCEPTION_RESOURCE_NOT_FOUND = "exception.resource.not-found";
+    public static final String EXCEPTION_DATA_INTEGRITY_VIOLATION = "exception.data-integrity-violation";
+    public static final String EXCEPTION_DATA_INTEGRITY_VIOLATION_CONSTRAINT_VIOLATION = "exception.data-integrity-violation.constraint-violation";
+    public static final String EXCEPTION_METHOD_ARGUMENT_TYPE_MISMATCH = "exception.method-argument-type-mismatch";
+    public static final String EXCEPTION_ILLEGAL_ARGUMENT = "exception.illegal-argument";
     public static final String VALIDATION_ERROR = "validation.error";
 
     private ErrorCode() {
