7602a3cb1681445226e7d921223e9fa9cb07c985
Asraf Uddin Ahmed <aahmed@ejogajogbd.com>
2021-12-03 15:45:56 +0600

    Replace getter and setter methods in place of lombok Data annotation

--- src/main/java/com/smefinance/todo/entities/TodoTask.java
+++ src/main/java/com/smefinance/todo/entities/TodoTask.java
@@ -1,67 +1,112 @@
 package com.smefinance.todo.entities;
 
-import java.util.Date;
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.EnumType;
-import javax.persistence.Enumerated;
-import javax.persistence.GeneratedValue;
-import static javax.persistence.GenerationType.IDENTITY;
-import javax.persistence.Id;
-import javax.persistence.PrePersist;
-import javax.persistence.PreUpdate;
-import javax.persistence.Table;
-import javax.persistence.Temporal;
-import javax.persistence.TemporalType;
-
 import com.smefinance.todo.contants.PriorityType;
 import com.smefinance.todo.contants.TaskStatus;
 
-import lombok.Data;
+import javax.persistence.*;
+import java.util.Date;
+
+import static javax.persistence.GenerationType.IDENTITY;
 
 @Entity
 @Table(name = "todo_task")
-@Data
 public class TodoTask {
 
-	@Id
-	@GeneratedValue(strategy = IDENTITY)
-	@Column(name = "id", unique = true, nullable = false)
-	private Integer id;
+    @Id
+    @GeneratedValue(strategy = IDENTITY)
+    @Column(name = "id", unique = true, nullable = false)
+    private Integer id;
 
-	@Column(name = "title", nullable = false, length = 1000)
-	private String title;
-	
-	@Column(name = "description", columnDefinition="TEXT")
-	private String description;
-	
-	@Column(name = "priority", nullable = false, columnDefinition="ENUM('LOW', 'MEDIUM', 'HIGH')")
-	@Enumerated(EnumType.STRING)
-	private PriorityType priority;
-	
-	@Column(name = "status", nullable = false, columnDefinition="ENUM('TODO', 'DOING', 'DONE')")
-	@Enumerated(EnumType.STRING)
-	private TaskStatus status;
-	
-	@Column(name = "is_deleted", nullable = false)
-	private boolean isDeleted;
-	
-	@Temporal(TemporalType.TIMESTAMP)
-	@Column(name = "created_at", nullable = false, length = 19)
-	private Date createdAt;
-	
-	@Temporal(TemporalType.TIMESTAMP)
-	@Column(name = "modified_at", nullable = false, length = 19)
-	private Date modifiedAt;
-	
-	@PrePersist
-	protected void onCreate() {
-		createdAt = new Date();
-		modifiedAt = new Date();
-	}
+    @Column(name = "title", nullable = false, length = 1000)
+    private String title;
 
-	@PreUpdate
-	protected void onUpdate() {
-		modifiedAt = new Date();
-	}
+    @Column(name = "description", columnDefinition = "TEXT")
+    private String description;
+
+    @Column(name = "priority", nullable = false, columnDefinition = "ENUM('LOW', 'MEDIUM', 'HIGH')")
+    @Enumerated(EnumType.STRING)
+    private PriorityType priority;
+
+    @Column(name = "status", nullable = false, columnDefinition = "ENUM('TODO', 'DOING', 'DONE')")
+    @Enumerated(EnumType.STRING)
+    private TaskStatus status;
+
+    @Column(name = "is_deleted", nullable = false)
+    private boolean isDeleted;
+
+    @Temporal(TemporalType.TIMESTAMP)
+    @Column(name = "created_at", nullable = false, length = 19)
+    private Date createdAt;
+
+    @Temporal(TemporalType.TIMESTAMP)
+    @Column(name = "modified_at", nullable = false, length = 19)
+    private Date modifiedAt;
+
+    @PrePersist
+    protected void onCreate() {
+        createdAt = new Date();
+        modifiedAt = new Date();
+    }
+
+    @PreUpdate
+    protected void onUpdate() {
+        modifiedAt = new Date();
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public PriorityType getPriority() {
+        return priority;
+    }
+
+    public void setPriority(PriorityType priority) {
+        this.priority = priority;
+    }
+
+    public TaskStatus getStatus() {
+        return status;
+    }
+
+    public void setStatus(TaskStatus status) {
+        this.status = status;
+    }
+
+    public boolean isDeleted() {
+        return isDeleted;
+    }
+
+    public void setDeleted(boolean deleted) {
+        isDeleted = deleted;
+    }
+
+    public Date getCreatedAt() {
+        return createdAt;
+    }
+
+    public Date getModifiedAt() {
+        return modifiedAt;
+    }
+
 }
